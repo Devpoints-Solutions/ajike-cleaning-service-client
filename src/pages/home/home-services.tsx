f31dd6b6eb83bc925865be0267e3b45f4fd5cff1
certifiedTboy <etosin70@gmail.com>
2026-08-24 05:02:25 +0100

    update

--- src/pages/home/home-services.tsx
+++ src/pages/home/home-services.tsx
@@ -51,7 +51,7 @@ function HomeServices() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
         {SERVICES?.slice(0, 6).map((service, index) => (
           <Link
             href="/auth/sign-in"
