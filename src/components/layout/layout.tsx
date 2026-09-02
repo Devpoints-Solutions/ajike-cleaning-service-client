7988c6363e77340a09aceadf21b27b4595fe991b
certifiedTboy <etosin70@gmail.com>
2026-09-02 23:15:47 +0100

    add dymaic page title

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,12 +3,14 @@ import AppRoutes from "./app-routes";
 import Header from "./header";
 import Footer from "@/components/layout/footer";
 import { Toaster } from "@/components/ui/toaster";
+import { PageMetadata } from "../common/page-meta-data";
 
 function Layout() {
   const [pathname] = useLocation();
 
   return (
     <>
+      <PageMetadata />
       {!["auth", "dashboard", "admin"].includes(pathname.split("/")[1]) && (
         <Header onRequest={() => {}} />
       )}
